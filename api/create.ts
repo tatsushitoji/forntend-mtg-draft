00968b454c7a72bd81ab5940d0033f549d2f4e5f
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2019-12-17 19:50:42 +0900

    feat: create draft

--- api/create.ts
+++ api/create.ts
@@ -14,10 +14,22 @@ export default async (req: NowRequest, res: NowResponse) => {
     headless: chrome.headless,
 })
   const page = await browser.newPage()
-  await page.goto('https://hackmd.io/')
-  const coverHeading = await page.$('.cover-heading')
-  const coverheadingText = await (await coverHeading.getProperty('textContent')).jsonValue()
+  await page.goto('https://hackmd.io/login')
+  await page.type('input[name="email"]', process.env.MAIL_ADRESS)
+  await page.type('input[name="password"]', process.env.PASSWORD)
+  await page.click('#login > .user-container > .form-horizontal > div > .btn')
+  await page.waitForNavigation()
+
+  await page.goto('https://hackmd.io/?nav=overview&template=3f8f6b87-807f-40c7-a49c-3dcc9ded007b')
+  const button = await page.waitForSelector('.modal-body > .row > .col-sm-4 > .ui-templates-container > .btn-primary', { visible: true })
+  await button.click()
+  await page.waitForNavigation()
+  await page.waitForSelector('h1#フロントエンドMTG', { visible: true })
+
+  const pageURL = page.url()
+  // const name = await page.$('.ui-name')
+  // const nameText = await (await name.getProperty('textContent')).jsonValue()
   await browser.close()
-  res.json({headText: coverheadingText})
+  res.json({url: pageURL})
 }
 
